f06f35017b17a3ac88affc2e6e27b6163c9935d8
omdwd74 <omjidwivedi123@gmail.com>
2023-09-29 02:56:44 +0530

    add partials

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express();
 const path = require('path')
+const ejsMate = require('ejs-mate');
 
-app.get('/',(req,res)=>{
-    res.send('<h1>Welcome</h1>');
+
+app.engine('ejs',ejsMate)
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'))
+
+
+
+app.get('/', async(req,res)=>{
+    res.render('wool_grounds/home');
+
+})
+app.get('/about',async(req,res)=>{
+    res.render('wool_grounds/about');
 
 })
 app.listen(3000,()=>{
